64c991aaa3a7dcc4bd3bdd18b4ff398ce1f4b746
Chinmay Rokade <chinmayrokade45@gmail.com>
2026-08-01 12:06:47 +0530

    Add just code

---
+++ src/com/pratice/Test.java
@@ -0,0 +1,15 @@
+package com.pratice;
+
+import java.util.List;
+
+public class Test {
+
+	public static void main(String[] args) {
+		List<Integer> list = List.of(2,5,6,9,8,3,4);
+		
+		
+		//list.stream()
+
+	}
+
+}
